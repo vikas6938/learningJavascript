410096e279572310d720c99165e652684198500d
vikas Borse <vikasborse000@gmail.com>
2024-12-10 02:43:06 +0530

    Operations.js Added

---
+++ Javascript/basic/Operations.js
@@ -0,0 +1,45 @@
+ 
+
+//  --------------------------------------Operation
+
+// --------------Negative Value
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// ------------------Arithmetic
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*3);
+
+// -------------------MergeString
+let str1 = "hello"
+let str2 = "vikas"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);        ----> 12
+// console.log(1 + "2");        ----> 12
+// console.log("1" + 2 + 2);    ----> 122 
+// console.log(1 + 2 + "2");    ----> 32
+
+
+// ------------------- Negative Positive vallue
+
+// console.log(-true);  ------>-1
+// console.log(+true);  ------> 1
+
+// -------------------
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// ---------------------Prefix & Postfix value
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
+
