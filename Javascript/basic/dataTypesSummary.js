363f4daa50c5d5c7657f6bfac7dcbcc517ed5b39
vikas Borse <vikasborse000@gmail.com>
2024-12-10 05:13:27 +0530

    dataTypeSummary

---
+++ Javascript/basic/dataTypesSummary.js
@@ -0,0 +1,14 @@
+// -----Primitive DataTypes------------>
+
+// ---- 7 Types  : -> 
+//    String, 
+//    Number, 
+//    Buolean, 
+//    Null -> Blank, 
+//    undefined, -> Memory declare he - value declare he but value is empty
+//    Symbol, -> kisi bhi value ko unique banane ke liye
+//    BigInt, -> jyada Badi value handel nhi hoti vah like scientific value
+//    Reference (Non Premitive value)
+//    Array, Object, Functions
+
+
