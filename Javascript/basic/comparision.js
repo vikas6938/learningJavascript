036369844d2af360acf45679b0ec18ff85fbd37b
vikas Borse <vikasborse000@gmail.com>
2024-12-10 02:56:05 +0530

    comparision task added

---
+++ Javascript/basic/comparision.js
@@ -0,0 +1,23 @@
+// ----------Comparision.js---------->
+
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// ----------------------TypeScript----->
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// -----------------------Comparision
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// ----------------------Equality Check
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
